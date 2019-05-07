d14b22e0353673ceabc8f4ae138ce8b03b511f4b
Sascha Metz <metz@raum3.de>
2019-05-07 19:06:06 +0200

    changing routes at custom endpoints

--- wp-content/themes/hwpgrossmann/includes/custom_rest/routes.php
+++ wp-content/themes/hwpgrossmann/includes/custom_rest/routes.php
@@ -2,7 +2,7 @@
 /** Custom Rest Routes for hwpgrossmann */
 add_action( 'rest_api_init', function () {
   // Get Options
-  register_rest_route( HWPGROSSMANN_REST_NAMESPACE, '/options', array(
+  register_rest_route( HWPGROSSMANN_REST_NAMESPACE, 'options', array(
     'methods' => 'GET',
     'callback' => 'handle_options',
   ) );
